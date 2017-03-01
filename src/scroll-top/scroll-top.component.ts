ad140e60a0cff5b6650300602ca38976cef38b5e
Laussel LOïc <loic.laussel@orange.com>
2017-03-01 16:44:56 +0100

    chore(copyright): fix copyright in file headers

--- src/scroll-top/scroll-top.component.ts
+++ src/scroll-top/scroll-top.component.ts
@@ -1,6 +1,7 @@
 /*!
  * --------------------------------------------------------------------------
  * Ng-boosted - scroll-top.component.ts
+ * copyright 2017 Orange SA
  * Licensed under MIT (https://github.com/Orange-OpenSource/ng-boosted/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
