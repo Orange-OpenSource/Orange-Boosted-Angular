6bdbef40df5e0aa5e659f4f5ec8f6e098a5452be
Laussel LOïc <loic.laussel@orange.com>
2017-03-02 14:51:41 +0100

    chore(config): update config

--- dist/o-navbar/o-navbar.component.js
+++ dist/o-navbar/o-navbar.component.js
@@ -8,7 +8,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var TEST_PATTERN = /xs|sm|md|lg|xl/;
+var TEST_PATTERN = /xs|sm|md|lg|xl|xxl/;
 var ONavLink = (function () {
     function ONavLink() {
     }
@@ -48,7 +48,7 @@ __decorate([
 ONavbarComponent = __decorate([
     core_1.Component({
         selector: 'o-navbar',
-        template: "\n    <nav [class]=\"'navbar navbar-inverse ' + (pattern.test(breakpoint) ? 'navbar-toggleable-'+breakpoint : 'navbar-toggleable-sm')\" role=\"navigation\">\n        <div class=\"container\">\n            <a class=\"navbar-brand logo\" [routerLink]=\"homeRoute\">\n                <img src=\"/assets/img/orange-logo.jpg\" alt=\"Back to homepage\" title=\"Back to homepage\">\n            </a>\n            <button class=\"navbar-toggler\" type=\"button\" (click)=\"isCollapsed = !isCollapsed\"\n              [attr.aria-expanded]=\"!isCollapsed\" aria-controls=\"collapseExample\">\n              <span class=\"sr-only\">toggle navigation</span>\n              <span class=\"icon-menu\"></span>\n            </button>\n            <div class=\"navbar-collapse collapse\" id=\"collapsingNavbarHead\" [ngbCollapse]=\"isCollapsed\">\n              <ul class=\"navbar-nav\">\n                <li class=\"nav-item\" routerLinkActive=\"active\" *ngFor=\"let item of items\">\n                  <a class=\"nav-link\" [routerLink]=\"item.route\">{{item.title}}</a>\n                </li>\n              </ul>\n            </div>\n        </div>\n    </nav>\n  "
+        template: "\n    <nav [class]=\"'navbar navbar-inverse ' + (pattern.test(breakpoint) ? 'navbar-toggleable-'+breakpoint : 'navbar-toggleable-sm')\" role=\"navigation\">\n        <div class=\"container\">\n            <a class=\"navbar-brand logo\" [routerLink]=\"homeRoute\">\n                <img src=\"assets/img/orange-logo.jpg\" alt=\"Back to homepage\" title=\"Back to homepage\">\n            </a>\n            <button class=\"navbar-toggler\" type=\"button\" (click)=\"isCollapsed = !isCollapsed\"\n              [attr.aria-expanded]=\"!isCollapsed\" aria-controls=\"collapseExample\">\n              <span class=\"sr-only\">toggle navigation</span>\n              <span class=\"icon-menu\"></span>\n            </button>\n            <div class=\"navbar-collapse collapse\" id=\"collapsingNavbarHead\" [ngbCollapse]=\"isCollapsed\">\n              <ul class=\"navbar-nav\">\n                <li class=\"nav-item\" routerLinkActive=\"active\" *ngFor=\"let item of items\">\n                  <a class=\"nav-link\" [routerLink]=\"item.route\">{{item.title}}</a>\n                </li>\n              </ul>\n            </div>\n        </div>\n    </nav>\n  "
     })
 ], ONavbarComponent);
 exports.ONavbarComponent = ONavbarComponent;
