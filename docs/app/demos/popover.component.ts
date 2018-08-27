279934068b04b3db8d5a09cda3850e07fa289c31
Lionel MUKUNA CIOWELA <lionel.mukunaciowela@orange.com>
2018-08-27 10:40:57 +0200

    adding margin for buttons

--- docs/app/demos/popover.component.ts
+++ docs/app/demos/popover.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component ({
     selector: 'demo-popover',
+    styles: [`
+    button {
+        margin-right: 0.5rem;
+    }
+    `],
     template: require('./popover.component.html')
 })
 export class DemoPopover { }
--- docs/app/demos/tooltip.component.ts
+++ docs/app/demos/tooltip.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'demo-tooltip',
+    styles: [`
+    button {
+        margin-right: 0.5rem;
+    }
+    `],
     template: require('./tooltip.component.html')
 })
 export class DemoTooltip { }
