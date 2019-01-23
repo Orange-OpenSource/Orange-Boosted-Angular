00c5b1f3308a64e6e21035c1ffbb181e86dd82a2
Lionel MUKUNA CIOWELA <lionel.mukunaciowela@orange.com>
2019-01-23 15:58:35 +0100

    updates package and version

--- docs/app/global.ts
+++ docs/app/global.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '4.0.1-beta.0';
+    public ngBoostedVersion = '4.0.1-beta.1';
     public boostedVersion = '4.1.3';
     public angularVersion = '7.0.0' ;
     public ngBootstrapVersion = '4.0.1';
