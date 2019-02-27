16668b8d642f704243e238537e73d3f1b8768031
Lionel MUKUNA CIOWELA <lionel.mukunaciowela@orange.com>
2019-02-27 11:12:29 +0100

    creating V4.0.2

--- docs/app/global.ts
+++ docs/app/global.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '4.0.2-beta.0';
+    public ngBoostedVersion = '4.0.2';
     public boostedVersion = '4.2.2';
     public angularVersion = ' > 7.2.0' ;
     public ngBootstrapVersion = '4.0.2';
