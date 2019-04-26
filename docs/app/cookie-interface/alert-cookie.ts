5161871d33b86334036a1b47aa173f2aa1007466
Lionel Mukuna Ciowela <lionel.mukunaciowela@orange.com>
2019-04-26 18:02:14 +0200

    fix(cookie-consent): redefine and updating cookie function, css and html #18

--- docs/app/cookie-interface/alert-cookie.ts
+++ docs/app/cookie-interface/alert-cookie.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CookieManagerService } from '../util/cookie-utils';
 
@@ -9,55 +9,57 @@ interface Alert {
     allow: string;
 }
 
-const ALERTS: Alert[] = [{
-    type: 'success',
-    message: 'This is an success alert',
-    deny: 'Refuse Cookies',
-    allow: 'Accept Cookies',
-  }];
-
 @Component({
     selector: 'alert-cookie',
     templateUrl: './alert-cookie.html',
     styleUrls: ['./alert-cookie.css'],
-    providers: [CookieManagerService]
+    providers: [CookieManagerService, TranslateService]
 })
-export class AlertCookie implements OnInit {
-    private alert: Alert;
+export class AlertCookieService {
+
+    private cookieAlert: Alert = {
+        type: 'warning',
+        message: 'This is an success alert',
+        deny: 'Refuse Cookies',
+        allow: 'Accept Cookies',
+    };
     private GA_ID = 'test';
     private showAlertCookie: boolean = true;
 
     constructor(private translateService: TranslateService, private cookiemanager: CookieManagerService) {}
-    public ngOnInit() {
-        this.translateService.addLangs(['en', 'fr']);
-        this.translateService.setDefaultLang('en');
 
-        const browserLang = this.translateService.getBrowserLang();
-        this.translateService.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    public initAlertCookieService() {
+        if (this.showAlertCookie) {
+            this.translateService.addLangs(['en', 'fr']);
+            this.translateService.setDefaultLang('en');
 
-        this.translateService.get(['cookie.header', 'cookie.message', 'cookie.allow', 'cookie.deny']).subscribe((data) => {
-            this.alert.type = 'warning';
-            this.alert.message = data['cookie.message'];
-            this.alert.deny = data['cookie.deny'];
-            this.alert.allow = data['cookie.allow'];
-        });
+            const browserLang = this.translateService.getBrowserLang();
+            this.translateService.use(browserLang.match(/en|fr/) ? browserLang : 'en');
 
-        const TrackNavigator = navigator.doNotTrack;
-            // Check donotTrack and Cookie Consent and do nothing if those values are positive
-        if ( TrackNavigator === '1' || TrackNavigator === 'yes' || !TrackNavigator) {
-            this.cookiemanager.rejectCookie(this.GA_ID);
-            this.showAlertCookie = false;
-            return;
-        }
-        if ( !this.cookiemanager.getCookie('consent') && (TrackNavigator === '1' || TrackNavigator === 'yes' || !TrackNavigator)) {
-            this.cookiemanager.rejectCookie(this.GA_ID);
-            this.showAlertCookie = false;
-            return;
-        }
-        if (this.cookiemanager.getCookie('consent') === 'false') {
-        this.cookiemanager.rejectCookie(this.GA_ID);
-        this.showAlertCookie = false;
-        return;
+            this.translateService.get(['cookie.header', 'cookie.message', 'cookie.allow', 'cookie.deny']).subscribe((data) => {
+                this.cookieAlert.type = 'warning';
+                this.cookieAlert.message = data['cookie.message'];
+                this.cookieAlert.deny = data['cookie.deny'];
+                this.cookieAlert.allow = data['cookie.allow'];
+            });
+
+            const TrackNavigator = navigator.doNotTrack;
+                // Check donotTrack and Cookie Consent and do nothing if those values are positive
+            if ( TrackNavigator === '1' || TrackNavigator === 'yes' || !TrackNavigator) {
+                this.cookiemanager.rejectCookie(this.GA_ID);
+                this.showAlertCookie = false;
+                return;
+            }
+            if ( !this.cookiemanager.getCookie('consent') && (TrackNavigator === '1' || TrackNavigator === 'yes' || !TrackNavigator)) {
+                this.cookiemanager.rejectCookie(this.GA_ID);
+                this.showAlertCookie = false;
+                return;
+            }
+            if (this.cookiemanager.getCookie('consent') === 'false') {
+                this.cookiemanager.rejectCookie(this.GA_ID);
+                this.showAlertCookie = false;
+                return;
+            }
         }
     }
 
