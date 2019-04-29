803b90d9bef53289e8183306ac89a63281549ecb
Lionel Mukuna Ciowela <lionel.mukunaciowela@orange.com>
2019-04-29 11:53:36 +0200

    fix(cookie-consent): updating html and corecting functionnability #18

--- docs/app/cookie-interface/alert-cookie.ts
+++ docs/app/cookie-interface/alert-cookie.ts
@@ -1,25 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CookieManagerService } from '../util/cookie-utils';
-
-interface Alert {
-    type: string;
-    message: string;
-    deny: string;
-    allow: string;
-}
+import { Alert } from './alert';
 
 @Component({
     selector: 'alert-cookie',
     templateUrl: './alert-cookie.html',
-    styleUrls: ['./alert-cookie.css'],
+    styles: [`
+        .alert-cookie {
+            color: #fff;
+            background-color: #000;
+        }
+
+        .bottom {
+            bottom: 0;
+            left: 0;
+            position: fixed;
+            right: 0;
+        }
+
+        .alert {
+            margin-bottom: 0 !important
+        }
+    `],
     providers: [CookieManagerService, TranslateService]
 })
-export class AlertCookieService {
+export class AlertCookieComponent implements OnInit {
 
+    // keep refs to subscriptions to be able to unsubscribe later
     private cookieAlert: Alert = {
-        type: 'warning',
-        message: 'This is an success alert',
+        header: 'warning',
+        message: 'This website uses cookies to ensure you get the best experience on our website.',
         deny: 'Refuse Cookies',
         allow: 'Accept Cookies',
     };
@@ -28,40 +39,39 @@ export class AlertCookieService {
 
     constructor(private translateService: TranslateService, private cookiemanager: CookieManagerService) {}
 
-    public initAlertCookieService() {
-        if (this.showAlertCookie) {
-            this.translateService.addLangs(['en', 'fr']);
-            this.translateService.setDefaultLang('en');
+    public ngOnInit() {
+        this.translateService.addLangs(['en', 'fr']);
+        this.translateService.setDefaultLang('en');
+        const browserLang = this.translateService.getBrowserLang();
+        this.translateService.use(browserLang.match(/en|fr/) ? browserLang : 'en');
 
-            const browserLang = this.translateService.getBrowserLang();
-            this.translateService.use(browserLang.match(/en|fr/) ? browserLang : 'en');
-
-            this.translateService.get(['cookie.header', 'cookie.message', 'cookie.allow', 'cookie.deny']).subscribe((data) => {
-                this.cookieAlert.type = 'warning';
-                this.cookieAlert.message = data['cookie.message'];
-                this.cookieAlert.deny = data['cookie.deny'];
-                this.cookieAlert.allow = data['cookie.allow'];
-            });
-
-            const TrackNavigator = navigator.doNotTrack;
-                // Check donotTrack and Cookie Consent and do nothing if those values are positive
-            if ( TrackNavigator === '1' || TrackNavigator === 'yes' || !TrackNavigator) {
-                this.cookiemanager.rejectCookie(this.GA_ID);
-                this.showAlertCookie = false;
-                return;
-            }
-            if ( !this.cookiemanager.getCookie('consent') && (TrackNavigator === '1' || TrackNavigator === 'yes' || !TrackNavigator)) {
-                this.cookiemanager.rejectCookie(this.GA_ID);
-                this.showAlertCookie = false;
-                return;
-            }
-            if (this.cookiemanager.getCookie('consent') === 'false') {
-                this.cookiemanager.rejectCookie(this.GA_ID);
-                this.showAlertCookie = false;
-                return;
-            }
+        if (this.cookiemanager.getCookie('consent') === 'false' && this.showAlertCookie) {
+          this.translateService.get(['cookie.header', 'cookie.message', 'cookie.allow', 'cookie.deny']).subscribe((data) => {
+              this.cookieAlert.header = data['cookie.header'];
+              this.cookieAlert.message = data['cookie.message'];
+              this.cookieAlert.deny = data['cookie.deny'];
+              this.cookieAlert.allow = data['cookie.allow'];
+          });
+          const TrackNavigator = navigator.doNotTrack;
+              // Check donotTrack and Cookie Consent and do nothing if those values are positive
+          if ( (TrackNavigator === '1' || TrackNavigator === 'yes' ) ) {
+              this.cookiemanager.rejectCookie(this.GA_ID);
+              this.showAlertCookie = false;
+              return;
+          }
+          // tslint:disable-next-line:max-line-length
+          if ( this.cookiemanager.getCookie('consent') === 'false' && ( (TrackNavigator === '1' || TrackNavigator === 'yes' ) )) {
+              this.cookiemanager.rejectCookie(this.GA_ID);
+              this.showAlertCookie = false;
+              return;
+          }
+          if ( this.cookiemanager.getCookie('consent') === 'false' ) {
+              this.cookiemanager.rejectCookie(this.GA_ID);
+              this.showAlertCookie = false;
+              return;
+          }
         }
-    }
+      }
 
     public deny() {
         this.cookiemanager.rejectCookie(this.GA_ID);
