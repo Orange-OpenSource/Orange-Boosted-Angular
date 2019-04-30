9445dfd761ad4ef891585f10eb5eaa38967402c1
MUKUNA CIOWELA Lionel <SVZV6149@ad-subs.w2k.francetelecom.fr>
2019-04-30 10:37:26 +0200

    fix(update-packages): updates of packages and moving dependecies to devdep #39

--- docs/app/global.ts
+++ docs/app/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '4.0.2';
-    public boostedVersion = '4.2.2';
+    public ngBoostedVersion = '4.0.3-beta.0';
+    public boostedVersion = '4.3.1';
     public angularVersion = ' > 7.2.0' ;
-    public ngBootstrapVersion = '4.0.2';
+    public ngBootstrapVersion = '4.0.3';
 }
