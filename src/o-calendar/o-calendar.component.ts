ae1b40d8881b8d7dce5563e9db24a9c0b5a2e141
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-18 11:43:18 +0200

    fix(calendar): adding title attributes into input and button #31

--- src/o-calendar/o-calendar.component.ts
+++ src/o-calendar/o-calendar.component.ts
@@ -49,17 +49,17 @@ import { isNumber, toInteger } from '../util/check';
     <form class="form-inline">
         <div class="form-group">
             <div class="input-group">
-                <input (ngModelChange)="onChange(bdatepicker.value); this.focusCalendar()" class="form-control calendar-input" #bdatepicker [footerTemplate]="footerTemplate" placeholder="{{placeHolder}}" name="dp" [(ngModel)]="model" ngbDatepicker #d="ngbDatepicker" attr.aria-label="{{labelInput}}">
+                <input (ngModelChange)="onChange(bdatepicker.value); this.focusCalendar()" class="form-control calendar-input" #bdatepicker [footerTemplate]="footerTemplate" placeholder="{{placeHolder}}" name="dp" [(ngModel)]="model" ngbDatepicker #d="ngbDatepicker" attr.title="{{labelInput}}" attr.aria-label="{{labelInput}}">
                 <div class="input-group-append">
-                    <button class="btn btn-outline-secondary calendar-{{color}}" (click)="d.toggle()" type="button" id="button-toggle" attr.aria-label="{{labelButton}}" aria-haspopup="true"></button>
+                    <button class="btn btn-outline-secondary calendar-{{color}}" (click)="d.toggle()" type="button" id="button-toggle" attr.title="{{labelButton}}" attr.aria-label="{{labelButton}}" aria-haspopup="true"></button>
                 </div>
             </div>
         </div>
     </form>
     <ng-template #footerTemplate>
         <hr class="my-0">
-        <button class="btn btn-custom-color btn-sm m-2 float-left" attr.aria-label="{{labelFooterToday}}" (click)="model = today; this.childEvent.emit(today); d.close(); this.focusCalendar();">Today</button>
-        <button class="btn btn-secondary btn-sm m-2 float-right" attr.aria-label="{{labelFooterClose}}" (click)="d.close(); this.focusCalendar();">Close</button>
+        <button class="btn btn-custom-color btn-sm m-2 float-left" attr.title="{{labelFooterToday}}" attr.aria-label="{{labelFooterToday}}" (click)="model = today; this.childEvent.emit(today); d.close(); this.focusCalendar();">Today</button>
+        <button class="btn btn-secondary btn-sm m-2 float-right" attr.title="{{labelFooterClose}}" attr.aria-label="{{labelFooterClose}}" (click)="d.close(); this.focusCalendar();">Close</button>
     </ng-template>
 `,
     providers: [{provide: NgbDateParserFormatter, useClass: DateParserFormatter}]
