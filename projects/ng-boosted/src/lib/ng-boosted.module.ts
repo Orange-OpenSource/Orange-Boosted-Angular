c286449a609a768c5bc4d8bc0c5b7485c51f092d
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-24 16:24:31 +0200

    fix(module): adding router module, common module and forms module

--- projects/ng-boosted/src/lib/ng-boosted.module.ts
+++ projects/ng-boosted/src/lib/ng-boosted.module.ts
@@ -1,4 +1,6 @@
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgBoostedComponent } from './ng-boosted.component';
 import { ScrollTopComponent } from './o-scroll/scroll-top.component';
@@ -10,7 +12,7 @@ const NG_BOOSTED_DECLARATIONS = [
 
 @NgModule({
   declarations: [NG_BOOSTED_DECLARATIONS],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   exports: [NG_BOOSTED_DECLARATIONS]
 })
 export class NgBoostedModule { }
