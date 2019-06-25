3e876ed96d487724db6343133b82a33c58e46b20
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-25 09:56:49 +0200

    fix(api): exporting ng boosted component : - Switch component - Scroll component - Nav bar component - Nav Link component

--- projects/ng-boosted/src/public_api.ts
+++ projects/ng-boosted/src/public_api.ts
@@ -6,4 +6,8 @@ export * from './lib/ng-boosted.service';
 export * from './lib/ng-boosted.component';
 export * from './lib/ng-boosted.module';
 
+// Ng-Boosted Export
+export * from './lib/o-nav-link/o-nav-link.component';
+export * from './lib/o-navbar/o-navbar.component';
 export * from './lib/o-scroll/scroll-top.component';
+export * from './lib/o-switch/switch.component';
