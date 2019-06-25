5ab3539c0d2f88470b9666fb76a4c7c2ff1749c9
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-25 13:03:57 +0200

    fix(package): updating angular and version of ng boosted

--- docs/app/global.ts
+++ docs/app/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '4.2.0';
+    public ngBoostedVersion = '4.2.1-beta.0';
     public boostedVersion = '4.3.1';
     public angularVersion = ' > 7.2.0' ;
-    public ngBootstrapVersion = '4.2.0';
+    public ngBootstrapVersion = '4.2.1';
 }
