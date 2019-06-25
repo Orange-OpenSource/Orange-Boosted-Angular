06210e9032ddf298a717bf418a985df481dd1aa4
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-25 13:50:56 +0200

    fix(packge): documentation verion update

--- docs/app/global.ts
+++ docs/app/global.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class Globals {
     public ngBoostedVersion = '4.2.1-beta.0';
     public boostedVersion = '4.3.1';
-    public angularVersion = ' > 7.2.0' ;
+    public angularVersion = ' > 8.0.0' ;
     public ngBootstrapVersion = '4.2.1';
 }
