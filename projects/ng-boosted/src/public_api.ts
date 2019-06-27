06fcb51b8140bbe95e7664dff3f409b7ad875ac7
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-27 18:14:12 +0200

    fix(public-api): exporting component

--- projects/ng-boosted/src/public_api.ts
+++ projects/ng-boosted/src/public_api.ts
@@ -11,3 +11,7 @@ export * from './lib/o-nav-link/o-nav-link.component';
 export * from './lib/o-navbar/o-navbar.component';
 export * from './lib/o-scroll/scroll-top.component';
 export * from './lib/o-switch/switch.component';
+export * from './lib/o-navbar/o-navbar.component';
+export * from './lib/o-carousel/o-carousel.component';
+export * from './lib/o-carousel/o-carousel-container/o-carousel-container.component';
+export * from './lib/o-dropdown/o-dropdown.component';
