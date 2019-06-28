baa4458e10ee7a8fb2a073b02da2d34c74db5cc7
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-28 17:22:09 +0200

    importing modules adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,119 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
+import { BoostwatchComponent } from './boostwatch/boostwatch.component';
+
+import { DocAccordion } from './docs/accordion.component';
+import { DocOCalendar } from './docs/o-calendar.component';
+import { DocModal } from './docs/modal.component';
+import { DocDropdown } from './docs/dropdown.component';
+import { DocTabs } from './docs/tabs.component';
+import { DocPagination } from './docs/pagination.component';
+import { DocTooltip } from './docs/tooltip.component';
+import { DocPopover } from './docs/popover.component';
+import { DocAlert } from './docs/alert.component';
+import { DocCarousel } from './docs/carousel.component';
+import { DocSwiper } from './docs/swiper.component';
+import { DocCollapse } from './docs/collapse.component';
+import { DocProgressBar } from './docs/progress-bar.component';
+import { DocONavbar } from './docs/o-navbar.component';
+import { DocOScrollTop } from './docs/o-scroll-top.component';
+import { DocOSwitch } from './docs/o-switch.component';
+import { DocOToggle } from './docs/o-toggle.component';
+
+const routes: Routes = [{
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'start',
+    component: StartComponent
+  },
+  {
+    path: 'documentation',
+    redirectTo: 'documentation/accordion',
+    pathMatch: 'full'
+  },
+  {
+    path: 'documentation/collapse',
+    component: DocCollapse
+  },
+  {
+    path: 'documentation/calendar',
+    component: DocOCalendar
+  },
+  {
+    path: 'documentation/accordion',
+    component: DocAccordion
+  },
+  {
+    path: 'documentation/modal',
+    component: DocModal
+  },
+  {
+    path: 'documentation/dropdown',
+    component: DocDropdown
+  },
+  {
+    path: 'documentation/tabs',
+    component: DocTabs
+  },
+  {
+    path: 'documentation/pagination',
+    component: DocPagination
+  },
+  {
+    path: 'documentation/tooltip',
+    component: DocTooltip
+  },
+  {
+    path: 'documentation/popover',
+    component: DocPopover
+  },
+  {
+    path: 'documentation/alert',
+    component: DocAlert
+  },
+  {
+    path: 'documentation/carousel',
+    component: DocCarousel
+  },
+  {
+    path: 'documentation/swiper',
+    component: DocSwiper
+  },
+  {
+    path: 'documentation/progress',
+    component: DocProgressBar
+  },
+  {
+    path: 'documentation/o-navbar',
+    component: DocONavbar
+  },
+  {
+    path: 'documentation/o-scroll-top',
+    component: DocOScrollTop
+  },
+  {
+    path: 'documentation/o-switch',
+    component: DocOSwitch
+  },
+  {
+    path: 'documentation/o-toggle',
+    component: DocOToggle
+  },
+  {
+    path: 'boostwatch',
+    component: BoostwatchComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
