f644bfcae73b9ea88eb1b2e76d9ae167c1d88cf5
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-06-28 11:04:56 +0200

    fix(module): adidng o nav menu and declaring component

--- projects/ng-boosted/src/public_api.ts
+++ projects/ng-boosted/src/public_api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of ng-boosted
  */
 
-export * from './lib/ng-boosted.service';
-export * from './lib/ng-boosted.component';
 export * from './lib/ng-boosted.module';
 
 // Ng-Boosted Export
@@ -12,6 +10,7 @@ export * from './lib/o-carousel/o-carousel-container/o-carousel-container.compon
 export * from './lib/o-carousel/o-carousel.component';
 export * from './lib/o-dropdown/o-dropdown.component';
 export * from './lib/o-nav-link/o-nav-link.component';
+export * from './lib/o-nav-menu/o-nav-menu.component';
 export * from './lib/o-navbar/o-navbar.component';
 export * from './lib/o-scroll/scroll-top.component';
 export * from './lib/o-switch/switch.component';
