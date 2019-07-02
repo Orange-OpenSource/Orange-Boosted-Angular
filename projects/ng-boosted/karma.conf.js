aad70aff98761482fd3e8412645d9d20a943dbea
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-07-02 14:21:55 +0200

    fix(karma): adding chrome headless

--- projects/ng-boosted/karma.conf.js
+++ projects/ng-boosted/karma.conf.js
@@ -22,11 +22,17 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     singleRun: false
   });
 };
