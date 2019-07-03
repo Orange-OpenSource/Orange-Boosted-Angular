8581c5bca38f130ba4a6604f3cc7f84cef487cc9
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-07-03 15:07:46 +0200

    adding testcomponent for testing purpose

---
+++ projects/ng-boosted/src/testing/test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-test',
+    template: `
+      <h1>This is a test Component</h1>
+    `
+  })
+  export class TestComponent {
+  }
