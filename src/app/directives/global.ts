ef7abefec866803e28d4421ccde09581bfc79217
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-07-08 09:52:12 +0200

    updating version of package for docs

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '4.2.1-beta.1';
+    public ngBoostedVersion = '4.2.1-beta.2';
     public boostedVersion = '4.3.1';
-    public angularVersion = ' > 8.2.0' ;
+    public angularVersion = ' > 7.0.0' ;
     public ngBootstrapVersion = '4.2.1';
 }
