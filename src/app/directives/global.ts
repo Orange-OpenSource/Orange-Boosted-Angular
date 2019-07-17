3d57a4b660903e81f5dcfc04c9adc7f447e93650
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-07-17 14:58:56 +0200

    updating global file

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '4.2.1';
+    public ngBoostedVersion = '5.0.0-beta.0';
     public boostedVersion = '4.3.1';
     public angularVersion = ' > 7.0.0' ;
-    public ngBootstrapVersion = '4.2.1';
+    public ngBootstrapVersion = '5.0.0';
 }
