e04be84d464679bc9707e786a49193b6f1fc762a
Mukuna Ciowela Lionel <lionel.mukunaciowela@orange.com>
2019-08-02 14:15:07 +0200

    fix(package): setting V5.1.0-beta.0

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '5.0.0';
+    public ngBoostedVersion = '5.1.0-beta.0';
     public boostedVersion = '4.3.1';
     public angularVersion = ' > 7.0.0' ;
-    public ngBootstrapVersion = '5.0.0';
+    public ngBootstrapVersion = '5.1.0';
 }
