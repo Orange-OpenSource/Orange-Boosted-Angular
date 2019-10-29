3e974ffc21b06896b3e702173d3061c36dfd198f
behnoosh <shiva.behnoosh@orange.com>
2019-10-29 15:18:36 +0100

    ngbModule added to navMenu

--- projects/ng-boosted/src/lib/o-nav-menu/o-nav-menu.module.ts
+++ projects/ng-boosted/src/lib/o-nav-menu/o-nav-menu.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ONavMenuComponent } from './o-nav-menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NgbModule],
   declarations: [ONavMenuComponent],
   exports: [ONavMenuComponent]
 })
