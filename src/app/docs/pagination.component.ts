91d2e8081a2c191bfa5987c4efa6120f27ee2199
behnoosh <shiva.behnoosh@orange.com>
2019-11-22 15:41:10 +0100

    pagination class changed (+1 squashed commits)
    Squashed commits:
    
    [166799c] class pagination add to doc
    
    (cherry picked from commit f74c5e14a4134e975f5ddefa0b59113f809edb5c)

--- src/app/docs/pagination.component.ts
+++ src/app/docs/pagination.component.ts
@@ -7,11 +7,15 @@ import { Analytics } from './shared/analytics';
     <docs-wrapper component="Pagination">
         <h3>Demo</h3>
         <demo-pagination></demo-pagination>
+        <h3>Usage</h3>
+        <div [innerHtml]=docContent></div>
         <code-box [snippets]="demoSnippets"></code-box>
     </docs-wrapper>
   `
 })
 export class DocPagination implements OnInit {
+  public docContent = require('html-loader!markdown-loader!./pagination.component.md');
+
   public demoSnippets = {
     markup: require('!!prismjs-loader?lang=html!../demos/pagination.component.html'),
     typescript: require('!!prismjs-loader?lang=typescript!../demos/pagination.component.ts')
