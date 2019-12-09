b3d8bcde5deec21370bb4692b996032bc6dc726c
behnoosh <shiva.behnoosh@orange.com>
2019-12-09 17:39:09 +0100

    correction date license (+1 squashed commits)
    Squashed commits:
    
    [95c9ed6] delete copyright

--- projects/ng-boosted/src/lib/o-scroll/scroll-top.component.ts
+++ projects/ng-boosted/src/lib/o-scroll/scroll-top.component.ts
@@ -1,7 +1,6 @@
 /*!
  * --------------------------------------------------------------------------
  * Ng-boosted - scroll-top.component.ts
- * copyright 2018 Orange SA
  * Licensed under MIT (https://github.com/Orange-OpenSource/ng-boosted/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
