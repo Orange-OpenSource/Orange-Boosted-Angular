bfd526d7aac1943d3e2e9b8a9579298ce4401ffb
behnoosh <shiva.behnoosh@orange.com>
2020-01-30 11:57:20 +0100

    doc changed for tooltip (+1 squashed commits)
    Squashed commits:
    
    [68624ee] doc changed for tooltip

---
+++ src/app/docs/tooltip.component.md
@@ -0,0 +1,3 @@
+#### Tooltip
+* `triggers` allows close and open tooltip with an event (ex: for mobile apps).
+* `autoClose` allows close tooltip by clicking `inside` or `outside` of button.
--- src/app/docs/tooltip.component.ts
+++ src/app/docs/tooltip.component.ts
@@ -7,11 +7,16 @@ import { Analytics } from './shared/analytics';
     <docs-wrapper component="Tooltip">
         <h3>Demo</h3>
         <demo-tooltip></demo-tooltip>
+        <br>
+        <br>
+        <h3>Usage</h3>
+        <div [innerHtml]=docContent></div>
         <code-box [snippets]="demoSnippets"></code-box>
     </docs-wrapper>
   `
 })
 export class DocTooltip implements OnInit {
+  public docContent = require('html-loader!markdown-loader!./tooltip.component.md');
   public demoSnippets = {
     markup: require('!!prismjs-loader?lang=html!../demos/tooltip.component.html'),
     typescript: require('!!prismjs-loader?lang=typescript!../demos/tooltip.component.ts')
