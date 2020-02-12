374464fae8cb1e635f656c5bf17635e60dfe0c04
Gaël Poupard <gael.poupard@orange.com>
2020-02-12 11:29:55 +0100

    feat(o-toggle): remove .o-switch class name to close #168

--- src/app/demos/o-toggle.component.ts
+++ src/app/demos/o-toggle.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'demo-o-toggle',
-    styles: [`
-        h4 {
-            margin-top: 2.5rem;
-        }
-    `],
     templateUrl: './o-toggle.component.html'
 })
 export class DemoOToggle {
