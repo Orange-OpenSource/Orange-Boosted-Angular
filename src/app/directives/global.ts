0b88b74f8a2fc7588f503a03b142b2cdfb28f006
behnoosh <shiva.behnoosh@orange.com>
2020-04-29 10:40:57 +0200

    feat(angular): update angular 8 to 9
    (cherry picked from commit 17a8c9823690594bf847e964de535d45a5824693)

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class Globals {
     public ngBoostedVersion = '5.2.2';
     public boostedVersion = '4.4.1';
-    public angularVersion = ' > 7.0.0' ;
-    public ngBootstrapVersion = '5.3.1';
+    public angularVersion = ' > 9.0.0' ;
+    public ngBootstrapVersion = '6.1.0';
 }
