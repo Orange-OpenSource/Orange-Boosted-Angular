ffae8803cfa489f4210eb3e11049f52468de015b
behnoosh <shiva.behnoosh@orange.com>
2020-06-02 15:27:07 +0200

    fix(navbar): aria-label, aria-current & active class added to dropdown

--- src/app/demos/navbar.component.ts
+++ src/app/demos/navbar.component.ts
@@ -4,4 +4,18 @@ import { Component } from '@angular/core';
   selector: 'demo-navbar',
   templateUrl: './navbar.component.html'
 })
-export class DemoNavbarComponent {}
+export class DemoNavbarComponent {
+  public active = false;
+  public languages = [
+    {name: 'EN', label: 'English version', lang: 'en', current: this.active}, 
+    {name: 'FR', label: 'Version française', lang: 'fr', current: this.active}, 
+    {name: 'SP', label: 'Versión en español', lang: 'sp', current: this.active}
+  ];
+  public selectLanuage = {name: 'Languages', label: '', lang: ''};
+
+  ChangeLanuage(l) { 
+    this.selectLanuage = l;
+    this.languages.forEach(lang => { lang.current = this.active });
+    l.current = !l.current;
+  }
+}
