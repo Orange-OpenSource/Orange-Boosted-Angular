86e20f9db8137c43397676c198d29a5d4fd33c97
behnoosh <shiva.behnoosh@orange.com>
2020-06-15 10:25:29 +0200

    version 5.2.3

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '5.2.2';
-    public boostedVersion = '4.4.1';
+    public ngBoostedVersion = '5.2.3';
+    public boostedVersion = '4.5.0';
     public angularVersion = ' > 7.0.0' ;
     public ngBootstrapVersion = '5.3.1';
 }
