6dd06eb4a69529124f0d7bcc2d842a9ee1602a63
behnoosh <shiva.behnoosh@orange.com>
2020-06-30 16:32:41 +0200

    version 6.1.0

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '6.0.0';
+    public ngBoostedVersion = '6.1.0';
     public boostedVersion = '4.5.0';
     public angularVersion = ' > 9.0.0' ;
-    public ngBootstrapVersion = '6.0.0';
+    public ngBootstrapVersion = '6.1.0';
 }
