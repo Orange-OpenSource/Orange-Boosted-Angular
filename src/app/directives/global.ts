a8e0359099b69e42ac5824ae6d15354bc452c0e6
behnoosh <shiva.behnoosh@orange.com>
2020-07-01 10:55:54 +0200

    version 6.1.0

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -5,5 +5,5 @@ export class Globals {
     public ngBoostedVersion = '6.0.0';
     public boostedVersion = '4.5.0';
     public angularVersion = ' > 9.0.0' ;
-    public ngBootstrapVersion = '6.0.0';
+    public ngBootstrapVersion = '6.1.0';
 }
