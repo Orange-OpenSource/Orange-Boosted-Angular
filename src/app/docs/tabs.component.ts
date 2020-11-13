7154c540565efa20ebb7abaf953847ece65fd58a
behnoosh <shiva.behnoosh@orange.com>
2020-11-13 15:39:57 +0100

    ngb-tabset deleted

--- src/app/docs/tabs.component.ts
+++ src/app/docs/tabs.component.ts
@@ -4,11 +4,14 @@ import { Component } from '@angular/core';
   selector: 'doc-tabs',
   template: `
     <docs-wrapper component="Tabs">
-    <p class="mt-4">Check the <a target="_blank" rel="noopener noreferrer"
-    href="https://ng-bootstrap.github.io/#/components/tabset/examples">ng-bootstrap's original component documentation</a>.</p>
-        <h3>Demo</h3>
-        <demo-tabs></demo-tabs>
-        <code-box [snippets]="demoSnippets"></code-box>
+      <p class="mt-4">Check the <a target="_blank" rel="noopener noreferrer"
+      href="https://boosted.orange.com/docs/4.5/components/nav">Boosted  original component documentation</a>.</p>
+      <p class="h6 text-primary">Warning ! : ngb-tabset no longer supported, available under version 7.1.1. Please use Nav.</p>
+      <p class="mt-4">Check the <a target="_blank" rel="noopener noreferrer"
+      href="https://ng-bootstrap.github.io/#/components/tabset">ngb-tabset</a>.</p>
+      <h3>Demo</h3>
+      <demo-tabs></demo-tabs>
+      <code-box [snippets]="demoSnippets"></code-box>
     </docs-wrapper>
   `
 })
