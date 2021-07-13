20ba8268e712b39a2f462ddd409b327ae0f4a994
behnoosh <shiva.behnoosh@orange.com>
2021-07-13 14:40:06 +0200

    migration boosted 4 to 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -224,7 +224,7 @@ const routes: Routes = [{
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/demos/toast.component.ts
+++ src/app/demos/toast.component.ts
@@ -6,22 +6,25 @@ import { ToastService } from './toast-service';
     templateUrl: './toast.component.html'
 })
 export class DemoToastComponent {
-  show = true;
-
+  show_toast_1 = true;
+  show_toast_2 = true;
+  
   constructor(public toastService: ToastService) {}
 
   close() {
-    this.show = false;
-    setTimeout(() => this.show = true, 5000);
+    this.show_toast_2 = false;
+    setTimeout(() => this.show_toast_2 = true, 3000);
   }
 
   showStandard() {
-    this.toastService.show('I am a standard toast', { classname: 'border border-primary fw-bold', delay: 10000 });
+    this.toastService.show('I am a standard toast', { classname: 'fw-bold'});
   }
+
   showSuccess() {
     this.toastService.show('I am a success toast', { classname: 'border border-success fw-bold', delay: 10000 });
   }
+
   showDanger() {
-    this.toastService.show('I am a danger toast', { classname: 'border border-danger fw-bold', delay: 15000 });
+    this.toastService.show('I am a danger toast',{ classname: 'border border-danger fw-bold', delay: 15000 });
   }
 }
