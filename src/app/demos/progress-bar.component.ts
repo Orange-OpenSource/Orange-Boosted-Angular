1a1a04764b0e52f86085802f2ce9917cfa37522d
laussel loïc <loic.laussel@orange.com>
2021-11-08 11:07:09 +0100

    remove node-sass and jquery

--- src/app/demos/progress-bar.component.ts
+++ src/app/demos/progress-bar.component.ts
@@ -2,14 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'demo-progress-bar',
-  styles: [`
-    >>> .progress {
-      margin-top: 1rem;
-    }
-    >>> .progress-bar {
-        color: white;
-    }
-  `],
   templateUrl: './progress-bar.component.html'
 })
 
