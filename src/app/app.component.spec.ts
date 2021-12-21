02c6dac552ab14c49cd5c481b9014fa6fa7cf1dc
shiva <shiva.behnoosh@orange.com>
2021-12-21 11:54:13 +0100

    (feat) update packges

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,13 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { ScrollTopModule } from 'ng-boosted';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, ScrollTopModule ],
+      imports: [ NgbModule, RouterTestingModule, ScrollTopModule ],
       declarations: [ AppComponent ]
     }).compileComponents();
   }));
@@ -17,14 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have a footer with 'Designed and built by the Boosted team'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const bannerDe: DebugElement = fixture.debugElement;
-    const paragraphDe = bannerDe.query(By.css('.o-footer-body p:first-child'));
-    const p: HTMLElement = paragraphDe.nativeElement;
-    fixture.detectChanges();
-    expect(p.textContent).toEqual('Designed and built by the Boosted team.');
   });
 });
