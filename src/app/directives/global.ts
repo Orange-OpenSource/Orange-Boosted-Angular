00d67b45aa08bf1f6ee1cc178267d40a3bb1e791
shiva <shiva.behnoosh@orange.com>
2022-02-24 15:28:43 +0100

    version 12.0.0
    12.0.0

--- src/app/directives/global.ts
+++ src/app/directives/global.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-    public ngBoostedVersion = '11.0.0';
-    public boostedVersion = '4.6.0';
+    public ngBoostedVersion = '12.0.0';
+    public boostedVersion = '5.0.0';
     public angularVersion = ' > 13.0.0' ;
-    public ngBootstrapVersion = '^11.0.0';
+    public ngBootstrapVersion = '^12.0.0';
 }
